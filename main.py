1592d7e5b737e00259208f9ff937252299c5544c
i.komarov <i.komarov@securitycode.ru>
2023-01-31 22:00:38 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from tkinter import ttk
+
+import random
+
+from colors import *
+
+window = Tk()
+window.title("Sorting Algorithms Visualization")
+window.maxsize(1000, 700)
+window.config(bg = WHITE)
+
+window.mainloop()
